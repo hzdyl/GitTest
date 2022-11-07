3a6ebd1fe4a906b6a96dcb1c15c4f92fb988c13f
我觉得其实OK <2066365950@qq.com>
2022-11-07 15:19:44 +0800

    第二次提交”
    git commit -m第二次提交”

---
+++ Assets/GitTestCode.cs
@@ -0,0 +1,14 @@
+/****************************************************
+    文件：GitTestCode.cs
+	作者：我觉得其实OK
+    邮箱: 2066365950@qq.com
+    日期：#CreateTime#
+	功能：GitHubTest
+*****************************************************/
+
+using UnityEngine;
+
+public class GitTestCode : MonoBehaviour 
+{
+    private string Git = "第一次测试";
+}
